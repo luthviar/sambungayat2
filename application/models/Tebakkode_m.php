c68c92be7127bbbf9f12ae577acbb979b0bffce5
Luthfi Abdurrahim <luthviar.a@gmail.com>
2017-11-11 22:32:17 +0700

    udah 4

--- application/models/Tebakkode_m.php
+++ application/models/Tebakkode_m.php
@@ -37,12 +37,43 @@ class Tebakkode_m extends CI_Model {
   }
 
   // Question
-  function getQuestion($questionNum){}
-
-  function isAnswerEqual($number, $answer){}
-
-  function setUserProgress($user_id, $newNumber){}
-
-  function setScore($user_id, $score){}
+  function getQuestion($questionNum)
+  {
+    $data = $this->db->where('number', $questionNum)
+      ->get('questions')
+      ->row_array();
+ 
+    if(count($data)>0) return $data;
+    return false;
+  }
+ 
+  function isAnswerEqual($number, $answer)
+  {
+    $this->db->where('number', $number)
+      ->where('answer', $answer);
+ 
+    if(count($this->db->get('questions')->row()) > 0)
+      return true;
+ 
+    return false;
+  }
+ 
+  function setUserProgress($user_id, $newNumber)
+  {
+    $this->db->set('number', $newNumber)
+      ->where('user_id', $user_id)
+      ->update('users');
+ 
+    return $this->db->affected_rows();
+  }
+ 
+  function setScore($user_id, $score)
+  {
+    $this->db->set('score', $score)
+      ->where('user_id', $user_id)
+      ->update('users');
+ 
+    return $this->db->affected_rows();
+  }
 
 }
